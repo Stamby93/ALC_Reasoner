d7955021bab3d87fa57286cf1fb39d68b1074631
Stamby <andrea.sca93@gmail.com>
2020-04-14 22:01:23 +0200

    Modificato il main in modo da poter effettuare delle prime prove di gestione del ontologia.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.*;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
@@ -10,20 +14,28 @@ public class main {
     public static void main(String[] args) throws Exception {
 
         OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-        OWLOntology ont = man.loadOntologyFromOntologyDocument(new File("pizza.owl.xml"));
+        OWLOntology ont = man.loadOntologyFromOntologyDocument(new File("prima.owl"));
 
-        OWLAxiom[] Concept = (OWLAxiom[]) ont.axioms().toArray();
-        int nConcept = Concept.length;
+        Set<OWLAxiom> expression = ont.axioms().collect(Collectors.toSet());
+        System.out.println("NUMERO ESPRESSIONI: " + expression.size());
+        int i = 0;
+        for (OWLAxiom e : expression ) {
+            i++;
+                System.out.println("Numero: " + i + " = " + e.toString());
+        }
+        //OWLAxiom Concept[] = (OWLAxiom[]) ont.axioms().toArray();
+        //int nConcept = Concept.length;
 
-        OWLReasonerFactory ReasonerFactory = new ALCReasonerFactory();
+        /*OWLReasonerFactory ReasonerFactory = new ALCReasonerFactory();
         OWLReasoner ALCReasoner = ReasonerFactory.createReasoner(null);
 
         for(int i=0; i< nConcept; i++){
             OWLClassExpression Expression = new OWLObjectIntersectionOfImpl(Concept[i].nestedClassExpressions());
             System.out.println("The concept number" + (i + 1) + "is" + ALCReasoner.isSatisfiable(Expression));
-
-
         }
+*/
+
+
 
     }
 
