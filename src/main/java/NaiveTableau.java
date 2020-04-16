e9dc00eb902bb55de7ec3e2712e75ed1941edff5
Stamby <andrea.sca93@gmail.com>
2020-04-16 18:09:32 +0200

    Implementazione regola intersezione e unione.
    Signed-off-by: Stamby <andrea.sca93@gmail.com>

--- src/main/java/NaiveTableau.java
+++ src/main/java/NaiveTableau.java
@@ -22,7 +22,7 @@ public class NaiveTableau implements Tableau {
 
 
     /**
-     * 
+     *
      */
     private OWLClassExpression Concept;
 
@@ -33,7 +33,7 @@ public class NaiveTableau implements Tableau {
     public boolean SAT() {
         Node root = NodeList.get(0);
         boolean result = false;
-        while(root.isWorking()){
+        while (root.isWorking()) {
             result = applyRule(root);
         }
 
@@ -41,11 +41,11 @@ public class NaiveTableau implements Tableau {
     }
 
     /**
-     * @param Concept 
+     * @param Concept
      * @return
      */
     public NaiveTableau(OWLClassExpression Concept) {
-        if(Concept == null)
+        if (Concept == null)
             throw new NullPointerException("Input concept must not be null");
         this.Concept = Concept;
         NodeList = new ArrayList<Node>();
@@ -54,19 +54,12 @@ public class NaiveTableau implements Tableau {
 
     /**
      * @param node
-     */
-    private void backtrack(Node node) {
-        // TODO implement here
-    }
-
-    /**
-     * @param node 
      * @return
      */
     private boolean applyRule(Node node) {
         OWLClassExpression rule = node.getWorkingRule();
         ClassExpressionType type = rule.getClassExpressionType();
-        switch (type){
+        switch (type) {
             case OBJECT_INTERSECTION_OF:
                 OWLObjectIntersectionOf intersection = (OWLObjectIntersectionOf) rule;
                 List<OWLClassExpression> disjointedList = intersection.operands().collect(Collectors.toList());
@@ -74,17 +67,17 @@ public class NaiveTableau implements Tableau {
                 return true;
             case OBJECT_UNION_OF:
                 OWLObjectUnionOf union = (OWLObjectUnionOf) rule;
-                List<OWLClassExpression> jointedtList = union.operands().collect(Collectors.toList());
-                node.setBranch(jointedtList);
-                if(node.applyChoice() == false)
+                List<OWLClassExpression> jointedList = union.operands().collect(Collectors.toList());
+                node.setBranch(jointedList);
+                if (!node.applyChoice())
                     return node.backtrack();
                 return true;
             case OBJECT_SOME_VALUES_FROM:
                 OWLObjectSomeValuesFrom someValue = (OWLObjectSomeValuesFrom) rule;
                 OWLClassExpression filler = someValue.getFiller();
                 //VERIFICARE LE PREMESSE PRIMA DI PROCEDERE CON L'APPLICAZIONE
-                NodeList.add(NodeList.size(),new Node(filler));
-                node.addRelation(someValue.getProperty(),NodeList.size());
+                NodeList.add(NodeList.size(), new Node(filler));
+                node.addRelation(someValue.getProperty(), NodeList.size());
                 break;
             case OBJECT_ALL_VALUES_FROM:
                 OWLObjectAllValuesFrom allValue = (OWLObjectAllValuesFrom) rule;
@@ -97,8 +90,10 @@ public class NaiveTableau implements Tableau {
                 break;
         }
 
-
+        return false;
     }
+
+
 
     /**
      * @return
