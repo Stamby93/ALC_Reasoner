65bcdc2c68cfd7f4477ec297e997e3b53302e4f7
andrea.santangelo95@gmail.com <229199199possE>
2020-04-19 13:56:24 +0200

    -Fatta un po' di pulizia al codice applicando i suggerimenti di intelliJ. Da NaiveTableau ho rimosso (come consigliato da intelliJ) gli attributi type e concept che non vengono utilizzati in effeti mai (type viene passato dal factory come null perchè giustamente a seconda del tipo scelto si chiama un costruttore diverso).
    -Adeguato il classDigram all'ultima versione
    -Autogenerato un sequenceDiagram per SAT tramite plugin di intelliJ (chiamato proprio sequenceDiagram)
    
    -Spostate le ontologie in una cartella Ontologie

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -36,70 +36,10 @@ public class main {
         assert expression != null;
         System.out.println("Concetto in input: " + expression.toString());
 
-        /*
-
-        if (expression.getClassExpressionType() == OBJECT_INTERSECTION_OF) {
-            OWLObjectIntersectionOf inter = (OWLObjectIntersectionOf) expression;
-
-            for (OWLClassExpression e : inter.operands().collect(Collectors.toSet())) {
-                System.out.println("Operand " + e.toString());
-            }
-        }
-
-        for (OWLClassExpression e : expression.nestedClassExpressions().collect(Collectors.toSet())) {
-            ClassExpressionType type = e.getClassExpressionType();
-            switch (type) {
-                case OWL_CLASS:
-                    System.out.println("Class: " + e.toString());
-                    break;
-                case OBJECT_SOME_VALUES_FROM:
-                    System.out.println("Object Some Value: " + e.toString());
-                    OWLObjectSomeValuesFrom casted = (OWLObjectSomeValuesFrom) e;
-                    System.out.println("Object some Value Filler: " + casted.getFiller().toString());
-                    break;
-                case OBJECT_ALL_VALUES_FROM:
-                    System.out.println("Object all Value: " + e.toString());
-                    OWLObjectAllValuesFrom casted2 = (OWLObjectAllValuesFrom) e;
-                    System.out.println("Object all Value Filler: " + casted2.getFiller().toString());
-                    break;
-                case OBJECT_HAS_VALUE:
-                    System.out.println("Object Has Value: " + e.toString());
-                    break;
-                case OBJECT_HAS_SELF:
-                    System.out.println("Object Has Self: " + e.toString());
-                    break;
-                case DATA_SOME_VALUES_FROM:
-                    System.out.println("Data Some Value: " + e.toString());
-                    break;
-                case DATA_ALL_VALUES_FROM:
-                    System.out.println("Data All Value: " + e.toString());
-                    break;
-                case DATA_HAS_VALUE:
-                    System.out.println("Data Has Value: " + e.toString());
-                    break;
-                case OBJECT_INTERSECTION_OF:
-                    System.out.println("Object Intersection: " + e.toString());
-                    break;
-                case OBJECT_UNION_OF:
-                    System.out.println("Object Union: " + e.toString());
-                    break;
-                case OBJECT_COMPLEMENT_OF:
-                    System.out.println("Object Complement of: " + e.toString());
-                    break;
-                case OBJECT_ONE_OF:
-                    System.out.println("Object One of: " + e.toString());
-                    break;
-                default:
-                    System.out.println("Unexpected Expression Type: " + e.toString());
-
-            }
-        }
-    */
-
-    OWLReasonerFactory ReasonerFactory = new ALCReasonerFactory();
-    OWLReasoner ALCReasoner;
-    ALCReasoner = ReasonerFactory.createReasoner(null);
-    System.out.println("The concept is "+ALCReasoner.isSatisfiable(expression.getNNF()));
+        OWLReasonerFactory ReasonerFactory = new ALCReasonerFactory();
+        OWLReasoner ALCReasoner;
+        ALCReasoner = ReasonerFactory.createReasoner(null);
+        System.out.println("The concept is "+ALCReasoner.isSatisfiable(expression.getNNF()));
 
     }
 
