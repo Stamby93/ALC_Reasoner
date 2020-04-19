37b291b686cf2101fd5175363317ad09f65252a4
andrea.santangelo95@gmail.com <229199199possE>
2020-04-19 13:34:31 +0200

    -Fatta un po' di pulizia al codice applicando i suggerimenti di intelliJ. Da NaiveTableau ho rimosso (come consigliato da intelliJ) gli attributi type e concept che non vengono utilizzati in effeti mai (type viene passato dal factory come null perchè giustamente a seconda del tipo scelto si chiama un costruttore diverso).
    -Adeguato il classDigram all'ultima versione
    -Autogenerato un sequenceDiagram per SAT tramite plugin di intelliJ (chiamato proprio sequenceDiagram)
    
    -Spostate le ontologie in una cartella Ontologie

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -13,7 +13,7 @@ public class main {
     public static void main(String[] args) throws Exception {
 
         OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-        OWLOntology ont = man.loadOntologyFromOntologyDocument(new File("06-06-26.owl"));
+        OWLOntology ont = man.loadOntologyFromOntologyDocument(new File("Ontologie/06-06-26.owl"));
         OWLDataFactory df = man.getOWLDataFactory();
         IRI iri = ont.getOntologyID().getOntologyIRI().get();
         OWLClass flag = df.getOWLClass(iri + "#assioma");
