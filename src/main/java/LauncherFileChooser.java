35b63e1e704ccdc163e8866adba5da6214d6b23c
vincenzo94 <vincenzomanfredi94@gmail.com>
2020-04-30 18:01:01 +0200

    CRC Cards

--- src/main/java/LauncherFileChooser.java
+++ src/main/java/LauncherFileChooser.java
@@ -160,7 +160,7 @@ public class LauncherFileChooser extends JPanel
             }
         }
     }
-    
+
     private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("ALC_REASONER - Manfredi - Santangelo - Scalella");
